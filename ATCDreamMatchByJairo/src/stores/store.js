9a6b520b9a06703a6ed103cfe18db6d80ffc836b
HolgadoJairoDavid <jairodavidholgado@gmail.com>
2024-06-24 00:46:53 -0300

    feat:(views) modal create team

--- ATCDreamMatchByJairo/src/stores/store.js
+++ ATCDreamMatchByJairo/src/stores/store.js
@@ -2,9 +2,13 @@ import {create} from 'zustand';
 
 const useTeamsStore = create((set) => ({
     teams: [],
+    addTeamIsOpen: false,
     setTeams: (teams) => set({ teams: teams }),
     removeAllTeams: () => set({ teams: [] }),
     addTeam: (team) => set((state) => ({ teams: [...state.teams, team] })),
+    removeTeam: (team) => set((state) => ({ teams: state.teams.filter((t) => t !== team) })),
+    openAddTeam: () => set({ addTeamIsOpen: true }),
+    closeAddTeam: () => set({ addTeamIsOpen: false }),
   }))
 
 export default useTeamsStore;
--- ATCDreamMatchByJairo/src/views/teams/Teams.jsx
+++ ATCDreamMatchByJairo/src/views/teams/Teams.jsx
@@ -1,25 +1,40 @@
 import style from './teams.module.css'
 import NavBar from '../../components/navBar/NavBar'
-// import SearchBar from '../../components/searchBar/SearchBar'
+import SearchBar from '../../components/searchBar/SearchBar'
 import useTeamsStore from '../../stores/store'
 
 const Teams = () => {
     const teams = useTeamsStore(state => state.teams)
+    const {addTeamIsOpen, openAddTeam, closeAddTeam} = useTeamsStore(state => state)
     return (
         <div>
             <NavBar />
-            {/* <SearchBar /> */}
             <div className={style.Teams}>
-                <h1>Teams</h1>
+                {!addTeamIsOpen && <h1>Teams</h1>}
                 <div className={style.TeamsContainer}>
-                    {teams.length > 0 ? teams.map((team, index) => (
+                    {teams.length > 0 && !addTeamIsOpen && teams.map((team, index) => (
                         <div key={index} className={style.TeamCard}>
                             <img src={team.img} alt={team.name} />
                             <h2>{team.name}</h2>
                             <h3>{team.league}</h3>
                             <p>{team.stadium}</p>
                         </div>
-                    )) : <h2>Parece que aún no tienes equipos. ¡Anímate a crearlos!</h2>}
+                    )) } 
+                    
+                    { (teams.length === 0 && !addTeamIsOpen) && <h2>Parece que aún no tienes equipos. ¡Anímate a crearlos!</h2>}
+                    {!addTeamIsOpen && <button className={style.AddTeamButton} onClick={() => openAddTeam()}>Add Team</button> }
+                    {
+                        addTeamIsOpen && (
+                            <div className={style.AddTeamModal}>
+                                <button className={style.CloseTeamButton} onClick={() => closeAddTeam()}>Close</button>
+                                <form>
+                                    <input type="text" placeholder="Team Name" />
+                                    <SearchBar />
+                                </form>
+                                <button>Save team</button>
+                            </div>
+                        )
+                    }
                 </div>
             </div>
         </div>
