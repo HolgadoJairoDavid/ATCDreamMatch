7c77c9f46bb596bc11b015cb936cc486f19ef1a9
HolgadoJairoDavid <jairodavidholgado@gmail.com>
2024-06-25 19:00:59 -0300

    feat:(components) confetti component

--- ATCDreamMatchByJairo/src/components/confetti/Confetti.jsx
+++ ATCDreamMatchByJairo/src/components/confetti/Confetti.jsx
@@ -1,8 +1,30 @@
+import React, { useEffect, useState } from 'react'
+import ReactConfetti from 'react-confetti'
+import style from './confetti.module.css'
+
 const Confetti = () => {
-  
+  const [windowDimensions, setWindowDimensions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight
+    })
+
+    const detectSize = () => {
+        setWindowDimensions({
+            width: window.innerWidth,
+            height: window.innerHeight
+        })
+    }
+    useEffect(() => {
+        window.addEventListener('resize', detectSize)
+        return () => window.removeEventListener('resize', detectSize)
+    }, [windowDimensions])
     return (
-        <div>
-          
+        <div className={style.ReactConfetti}>
+          <ReactConfetti
+          width={windowDimensions.width}
+          height={windowDimensions.height}
+          tweenDuration={2000}
+          />
         </div>
     )
 }
