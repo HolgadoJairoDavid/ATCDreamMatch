f872b8e87ff5fc02756798bf1b05ea69507a298d
HolgadoJairoDavid <jairodavidholgado@gmail.com>
2024-06-28 10:29:17 -0300

    fix:(platform) api axios env

--- ATCDreamMatchByJairo/src/lib/axios.js
+++ ATCDreamMatchByJairo/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_APP_API_URL,
+    baseURL: import.meta.env.VITE_APP_API_URL || "https://apiv3.apifootball.com",
 });
 export default api;
--- ATCDreamMatchByJairo/src/services/ClientService.js
+++ ATCDreamMatchByJairo/src/services/ClientService.js
@@ -2,6 +2,7 @@ import api from "../lib/axios";
 
 export default {
     searchPlayers(name) {
-        return api.get(`/?action=get_players&player_name=${name}&APIkey=${import.meta.env.VITE_APP_API_KEY}`);
+        const API_KEY = import.meta.env.VITE_APP_API_KEY || "ded47513a9b3291ada242205407f79d6723556c5c0c9b3da355558c73bd76235";
+        return api.get(`/?action=get_players&player_name=${name}&APIkey=${API_KEY}`);
     }
 };
